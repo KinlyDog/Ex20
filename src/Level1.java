935ab4f74a4c966aa739f820e5ef8201996b5014
Otryaskin Andrey <kinlydog@gmail.com>
2022-08-30 23:17:23 +1000

    Complete project

--- src/Level1.java
+++ src/Level1.java
@@ -1,5 +1,51 @@
 public class Level1 {
-    public static boolean white_walkers(String village) {
+    public static boolean walkers_search(String sub) {
+        int sum = 0;
 
+        for (int i = 0; i < sub.length(); i++) {
+            if (sub.charAt(i) == '=') {
+                sum++;
+            }
+        }
+
+        if (sum == 3) return true;
+
+        return false;
+    }
+
+    public static boolean white_walkers(String village) {
+        int startInd = -1;
+        int finishInd = -1;
+
+        int start = 0;
+        int finish = 0;
+
+        boolean flag = false;
+
+        for (int i = 0; i < village.length(); i++) {
+            if (village.charAt(i) > 47 && village.charAt(i) < 58) {
+                startInd = finishInd;
+                finishInd = i;
+
+                flag = true;
+            }
+
+            if (!flag || startInd == -1) {
+                flag = false;
+                continue;
+            }
+
+            start = Character.getNumericValue(village.charAt(startInd));
+            finish = Character.getNumericValue(village.charAt(finishInd));
+            String sub = village.substring(startInd, finishInd);
+
+            if (start + finish == 10 && !walkers_search(sub)) {
+                return false;
+            }
+
+            flag = true;
+        }
+
+        return flag;
     }
 }
